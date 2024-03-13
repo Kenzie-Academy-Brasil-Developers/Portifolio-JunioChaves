de5fc7d8f324038b23500779902d589aa1c2bdb3
Junio Chaves <68963163+13junio@users.noreply.github.com>
2024-03-13 07:57:23 -0300

    initial commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+    nameUser: "Junio Chaves",
+    githubUser: "tommil90",
+    linkedinUser: "tomasbenjamin",
+    whatsappNumber: "+5548988165891",
+    emailUser: "juniochavestj@gmail.com",
+    instagramUser: "",
+    facebookUser: "",
+  };
