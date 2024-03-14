a9e1395ce67c27bf63921dcf3c8d846bb7a1d444
Junio Chaves <68963163+13junio@users.noreply.github.com>
2024-03-13 22:23:34 -0300

    deletar

--- src/utils/userData.ts
+++
@@ -1,9 +0,0 @@
-export const userData = {
-    nameUser: "Junio Chaves",
-    githubUser: "tommil90",
-    linkedinUser: "tomasbenjamin",
-    whatsappNumber: "+5548988165891",
-    emailUser: "juniochavestj@gmail.com",
-    instagramUser: "",
-    facebookUser: "",
-  };
